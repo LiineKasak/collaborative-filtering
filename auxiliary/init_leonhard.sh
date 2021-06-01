cb37c67d2e44de81fac1dee446eca69f99366b03
Veronique Kaufmann <veronique.kaufmann@inf.ethz.ch>
2021-06-01 13:13:02 +0200

    add run scripts

--- auxiliary/init_leonhard.sh
+++ auxiliary/init_leonhard.sh
@@ -2,6 +2,16 @@
 # export PYTHONPATH=/path_to_source_directory/collaborative-filtering:$PYTHONPATH
 # python3 -m venv ~/venv
 
+directory_name=`pwd`
+export PYTHONPATH=$directory_name/../:$PYTHONPATH
+
+if [ -d "./venv" ]; then
+  source venv/bin/activate
+else
+  python3 -m venv ~/venv
+  source venv/bin/activate
+fi
+
 module load python_cpu/3.7.4 eth_proxy
 
 source venv/bin/activate
