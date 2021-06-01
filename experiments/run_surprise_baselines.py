5c5290a007e8a505905c3fad3fd577627cd1c4ef
Veronique Kaufmann <veronique.kaufmann@inf.ethz.ch>
2021-06-01 16:28:51 +0200

    don't run svd++, takes forever

--- experiments/run_surprise_baselines.py
+++ experiments/run_surprise_baselines.py
@@ -1,19 +1,16 @@
 import pandas as pd
-
 from auxiliary import data_processing
-from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, SVDpp, NMF, SlopeOne, CoClustering, KNNWithMeans, \
+from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF, SlopeOne, CoClustering, KNNWithMeans, \
     KNNWithZScore, KNNBaseline
 
 from src.surprise_baselines import SurpriseBaselines
 
 # # read the sample submission file to get the values we have to predict
-directory_path = data_processing.get_project_directory()
-data_pd = pd.read_csv(directory_path + '/data/data_train.csv')
-
+data_pd = data_processing.read_data()
 users, movies, predictions = data_processing.extract_users_items_predictions(data_pd)
 sub_users, sub_movies = data_processing.get_users_movies_to_predict()
 
-surprise_methods = [SVD, NormalPredictor, BaselineOnly, KNNBasic, SVDpp, NMF, SlopeOne, CoClustering, KNNWithMeans,
+surprise_methods = [SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF, SlopeOne, CoClustering, KNNWithMeans,
                     KNNWithZScore, KNNBaseline]
 
 for method in surprise_methods:
