e5ccd8b47b2c7cc429ec54f72d00158b82739d06
Martin Kucera <3159068+KuceraMartin@users.noreply.github.com>
2021-06-15 12:58:04 +0200

    CF-NADE: replace for-loops with matrix multiplications

--- src/models/cfnade.py
+++ src/models/cfnade.py
@@ -22,54 +22,47 @@ def mse_loss(predictions, target):
 class CFNADE(AlgoBase):
 
     class Model(nn.Module):
-        def __init__(self, number_of_items, number_of_scores):
+        def __init__(self, number_of_items, scores_tensor):
             super().__init__()
-            self.number_of_scores = number_of_scores
-            self.scores_tensor = torch.tensor(range(number_of_scores))
+            self.number_of_items = number_of_items
+            self.scores = scores_tensor
 
             self.hidden_units = 10
             self.hidden_bias = nn.Parameter(torch.rand(self.hidden_units, requires_grad=True))
-            self.hidden_W = nn.Parameter(torch.rand((number_of_scores, self.hidden_units, number_of_items), requires_grad=True))
+            self.hidden_W = nn.Parameter(torch.rand((scores_tensor.shape[0], number_of_items, self.hidden_units), requires_grad=True))
 
-            self.score_bias = nn.Parameter(torch.rand((number_of_scores, number_of_items), requires_grad=True))
-            self.score_V = nn.Parameter(torch.rand((number_of_scores, number_of_items, self.hidden_units), requires_grad=True))
+            self.score_bias = nn.Parameter(torch.rand((scores_tensor.shape[0], number_of_items), requires_grad=True))
+            self.score_V = nn.Parameter(torch.rand((scores_tensor.shape[0], number_of_items, self.hidden_units), requires_grad=True))
 
-        def hidden(self, histories):
-            res = self.hidden_bias.repeat(len(histories), 1)
-            for i, history in enumerate(histories):
-                for (item, rating) in history:
-                    res[i] += self.hidden_W[rating - 1, :, item]
-                res[i] = torch.tanh(res[i])
-            return res
+        def hidden(self, history: torch.Tensor):
+            biases = self.hidden_bias.repeat(history.shape[0], 1)
+            history_view = history.view(history.shape[0], self.scores.shape[0], 1, self.number_of_items)
+            return biases + torch.matmul(history_view, self.hidden_W).sum(axis=1).view(history.shape[0], self.hidden_units)
 
-        def score(self, item, history, k):
-            return self.score_bias[k, item] + batch_dot(self.score_V[k, item, :], self.hidden(history))
+        def dist(self, item: torch.Tensor, history: torch.Tensor):
+            v = self.score_V[:, item, :].view(self.scores.shape[0], item.shape[0], 1, self.hidden_units)
+            h = self.hidden(history).view(history.shape[0], self.hidden_units, 1)
+            dots = torch.matmul(v, h).view(self.scores.shape[0], item.shape[0])
+            scores = self.score_bias[:, item] + dots
+            return nn.Softmax(dim=0)(scores).T
 
-        def dist(self, item, history):
-            scores = torch.stack([self.score(item, history, k) for k in range(self.number_of_scores)])
-            return nn.Softmax(dim=0)(scores)
-
-        def forward(self, item, history):
+        def forward(self, item: torch.Tensor, history: torch.Tensor):
             d = self.dist(item, history)
-            res = d[0]
-            for k in range(1, self.number_of_scores):
-                res = res + (k + 1) * d[k]
-            return res
+            return torch.matmul(d, self.scores)
 
 
     def __init__(self, device, track_to_comet=False):
         AlgoBase.__init__(self, track_to_comet)
-        self.model = self.Model(self.number_of_movies, 5).to(device)
+        self.number_of_scores = 5
+        scores = torch.tensor(range(self.number_of_scores), device=device) + 1.0
+        self.model = self.Model(self.number_of_movies, scores).to(device)
         self.history = []
         self.device = device
 
     def make_history(self, users, movies, predictions):
-        self.history = [[] for u in range(self.number_of_users)]
+        self.history = torch.zeros((self.number_of_users, self.number_of_scores, self.number_of_movies), device=self.device)
         for (u, m, r) in zip(users, movies, predictions):
-            self.history[u].append((m, r))
-
-    def get_history(self, users):
-        return [self.history[user] for user in users]
+            self.history[u, r - 1, m] = 1
 
     def fit(self, users, movies, predictions, num_epochs=20, batch_size=128, learning_rate=1e-2):
         optimizer = optim.Adam(self.model.parameters(), lr=learning_rate)
@@ -93,14 +86,14 @@ class CFNADE(AlgoBase):
             for epoch in range(num_epochs):
                 for users_batch, movies_batch, target_predictions_batch in dataloader:
                     optimizer.zero_grad()
-                    predictions_batch = self.model(movies_batch, self.get_history(users_batch))
+                    predictions_batch = self.model(movies_batch, self.history[users_batch])
                     loss = mse_loss(predictions_batch, target_predictions_batch)
                     loss.backward()
                     optimizer.step()
                     pbar.update(1)
                     step += 1
                 with torch.no_grad():
-                    all_predictions = self.model(movies_test, self.get_history(users_test))
+                    all_predictions = self.model(movies_test, self.history[users_test])
                 reconstuction_rmse = data_processing.get_score(all_predictions.cpu().numpy(), pred_test.cpu().numpy())
                 pbar.set_description('At epoch {:3d} loss is {:.4f}'.format(epoch, reconstuction_rmse))
                 if reconstuction_rmse < best_rmse:
@@ -108,4 +101,4 @@ class CFNADE(AlgoBase):
         self.model.load_state_dict(res)
 
     def predict(self, users, movies):
-        return self.model(movies, self.get_history(users))
+        return self.model(movies, self.history[users])
