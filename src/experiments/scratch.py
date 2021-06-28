6deaabf56864135219def3625c278098d2087110
Veronique Kaufmann <veronique.kaufmann@inf.ethz.ch>
2021-06-28 17:50:29 +0200

    kaggle submission 0.99187

--- src/experiments/scratch.py
+++ src/experiments/scratch.py
@@ -1,10 +1,13 @@
 from sklearn.model_selection import train_test_split
+import numpy as np
 
+from models.svd_sgd import SVD_SGD
 from utils import data_processing, dataset, prediction_analysis
 from models.matrix_factorization import SVD
 from models.ncf import NCF
 from models.ensemble import Bagging
-from models.knn import KNN, KNNBag, KNNUserMovie
+from models.knn import KNN, KNNBag, KNNUserMovie, KNNSVD_Embeddings, KNNSVD_Biases
+from models.kmeans import KMeansRecommender
 """ 
     Run Cross-Validation for multiple approaches at the same time.
 
@@ -18,7 +21,6 @@ train_size = 0.9
 number_of_users = data_processing.get_number_of_users()
 number_of_movies = data_processing.get_number_of_movies()
 
-
 data_pd = data_processing.read_data()
 train_pd, test_pd = train_test_split(data_pd, train_size=train_size, random_state=42)
 
@@ -27,29 +29,105 @@ train_data_wrapper = dataset.DatasetWrapper(train_pd)
 test_users, test_movies, test_predictions = data_processing.extract_users_items_predictions(test_pd)
 train_data_wrapper.movies_per_user_representation()
 
+#
+# knn5 = KNN(n_neighbors=500, metric='cosine', method_name='KNN 5 cosine')
+# knn6 = KNN(n_neighbors=500, metric='euclidean', method_name='KNN 5 euclidean')
+# knn3 = KNN(n_neighbors=500, metric='manhattan', method_name='KNN 5 manhattan')
+# knn4 = KNN(n_neighbors=500, metric='chebyshev',method_name='KNN 5 chebyshev')
+#
+#
+# knnBag = KNNBag()
+#
+# knn_new = KNNUserMovie(n_user_neighbors=200, n_movie_neighbors=20)
+# knn_svd2_cosine_movie = KNNSVD_Embeddings(user_based=False, metric='cosine', n_neighbors=2,
+#                                           method_name="movie based KNNSVD 2 cosine")
+# knn_svd3_cosine_movie = KNNSVD_Embeddings(user_based=False, metric='cosine', n_neighbors=3,
+#                                           method_name="movie based KNNSVD 3 cosine")
+# knn_svd5_cosine_movie = KNNSVD_Embeddings(user_based=False, metric='cosine', n_neighbors=5,
+#                                           method_name="movie based KNNSVD 5 cosine")
+#
+# knn_svd2_euclidean_movie = KNNSVD_Embeddings(user_based=False, metric='euclidean', n_neighbors=2,
+#                                              method_name="movie based KNNSVD 2 euclidean")
+# knn_svd3_euclidean_movie = KNNSVD_Embeddings(user_based=False, metric='euclidean', n_neighbors=3,
+#                                              method_name="movie based KNNSVD 3 euclidean")
+# knn_svd5_euclidean_movie = KNNSVD_Embeddings(user_based=False, metric='euclidean', n_neighbors=5,
+#                                              method_name="movie based KNNSVD 5 euclidean")
+#
+# knn_svd2_cosine = KNNSVD_Embeddings(user_based=True, metric='cosine', n_neighbors=2,
+#                                     method_name="user based KNNSVD 2 cosine")
+# knn_svd3_cosine = KNNSVD_Embeddings(user_based=True, metric='cosine', n_neighbors=3,
+#                                     method_name="user based KNNSVD 3 cosine")
+# knn_svd5_cosine = KNNSVD_Embeddings(user_based=True, metric='cosine', n_neighbors=5,
+#                                     method_name="user based KNNSVD 5 cosine")
+#
+# knn_svd2_euclidean = KNNSVD_Embeddings(user_based=True, metric='euclidean', n_neighbors=2,
+#                                        method_name="user based KNNSVD 2 euclidean")
+# knn_svd3_euclidean = KNNSVD_Embeddings(user_based=True, metric='euclidean', n_neighbors=3,
+#                                        method_name="user based KNNSVD 3 euclidean")
+# knn_svd5_euclidean = KNNSVD_Embeddings(user_based=True, metric='euclidean', n_neighbors=5,
+#                                        method_name="user based KNNSVD 5 euclidean")
+# # knn_svd50 = KNNSVD_Embeddings(n_neighbors=50, method_name="KNNSVD 50")
+# knn_svd200 = KNNSVD_Embeddings(n_neighbors=200, method_name="KNNSVD 200")
 
-knn5 = KNN(n_neighbors=500, metric='cosine', method_name='KNN 5 cosine')
-knn6 = KNN(n_neighbors=500, metric='euclidean', method_name='KNN 5 euclidean')
-knn3 = KNN(n_neighbors=500, metric='manhattan', method_name='KNN 5 manhattan')
-knn4 = KNN(n_neighbors=500, metric='chebyshev',method_name='KNN 5 chebyshev')
-
-
-knnBag = KNNBag()
-
-knn_new = KNNUserMovie(n_user_neighbors=200, n_movie_neighbors=20)
 # approaches = [knn5, knn6, knn3, knn4]
-approaches = [knn_new]
-for knn in approaches:
-    print("================================")
-    print(knn.method_name)
-    print("================================\n")
-    knn.fit(train_data_wrapper)
-    pred = knn.predict(test_users, test_movies)
-    pa = prediction_analysis.PredictionAnalyser(test_users, test_movies, test_predictions, pred, knn=knn)
-    pa.analyze_prediction()
+#approaches = [knn_svd2_cosine_movie,
+              # knn_svd3_cosine_movie,
+              # knn_svd5_cosine_movie,
+              #
+              # knn_svd2_euclidean_movie,
+              # knn_svd3_euclidean_movie,
+              # knn_svd5_euclidean_movie,
+              #
+              # knn_svd2_cosine,
+              # knn_svd3_cosine,
+              # knn_svd5_cosine,
+              #
+              # knn_svd2_euclidean,
+              # knn_svd3_euclidean,
+              # knn_svd5_euclidean
+          #    ]
 
 
 
-# # rsmes = bag.cross_validate(data_pd)
-# # print(rsmes)
+#sv: 1, neighbors:  2
+
+
+# k = 10
+# epochs = 2
+# sgd = SVD_SGD(k_singular_values=k, epochs=epochs, verbal=True)
+#
+# rsmes = sgd.cross_validate(data_pd)
+# print(rsmes)
+
+svd = KNNSVD_Biases(k=10, n_neighbors=2)
+# rsmes = svd.cross_validate(data_pd)
+
+full_data_wrapper = dataset.DatasetWrapper(data_pd)
+
+svd.fit(data_wrapper=full_data_wrapper)
+svd.predict_for_submission("knn_svd")
+
+
+
+best_score = 5
+best_k = 0
+best_n = 0
+user_item = 'none'
+
+# for n in [1, 2, 5, 10, 50]:
+#     for k in range(1,12,2):
+#         print("================================")
+#         print("singular values: ", k, "neighbors: ", n)
+#         print("================================\n")
+#         knn = KNNSVD_Embeddings(k=k, user_based=True, metric='cosine', n_neighbors=n, method_name="user based KNNSVD 2 cosine")
+#
+#         rsmes = knn.cross_validate(data_pd)
+#         score = np.mean(rsmes)
+#         print("mean rsme: ", score)
+#
+#         if score < best_score:
+#             best_score = score
+#             best_k = k
+#             best_n = n
+#             user_item = 'user'
 
--- src/models/knn.py
+++ src/models/knn.py
@@ -3,7 +3,8 @@ from collections import defaultdict
 import numpy as np
 from sklearn.neighbors import NearestNeighbors, KNeighborsRegressor
 from tqdm import tqdm
-
+from sklearn.preprocessing import RobustScaler, PowerTransformer
+from src.models.svd import SVD
 from utils import data_processing, dataset
 import torch
 import torch.nn as nn
@@ -13,14 +14,191 @@ from scipy.sparse import csr_matrix
 
 from utils.dataset import DatasetWrapper
 from .algobase import AlgoBase
+from models.svd_sgd import SVD_SGD
 
 eps = 1e-6
+# [1.0008103659078311, 0.9991618105243478, 1.000329012060183, 0.9976463697503822, 0.9961922447259809]
+class KNNSVD_Biases(AlgoBase):
+    """ Prediction based on dimensionality reduction through singular value decomposition """
+
+    def __init__(self, k=10, svd_epochs=10, user_based=True, metric='cosine', algorithm='auto', n_neighbors=5, track_to_comet=False,
+                 method_name=None,
+                 api_key="rISpuwcLQoWU6qan4jRCAPy5s",
+                 projectname="cil-experiments", workspace="veroniquek", tag="baseline"):
+        AlgoBase.__init__(self, track_to_comet=track_to_comet, method_name=method_name, api_key=api_key,
+                          projectname=projectname, workspace=workspace, tag=tag)
+
+        self.n_neighbors = n_neighbors
+        self.knn = NearestNeighbors(metric=metric, algorithm=algorithm, n_neighbors=n_neighbors, n_jobs=-1)
+        self.data_wrapper = None
+        self.nearest_neighbors = None
+
+        self.k = k
+        self.user_based = user_based
+
+        self.svd = SVD_SGD(k_singular_values=k, epochs=10)
+
+        # self.transformer = RobustScaler(with_centering=False, with_scaling=False, unit_variance=False) # 1.0277 mean rsme
+        self.transformer = PowerTransformer()
+        print("using power transformer, k=", k, " epochs=", svd_epochs, "n = ", n_neighbors, " neighbors")
+
+    def compute_neighbors(self):
+
+        if self.user_based:
+            user_vectors = self.reconstructed_matrix  # shape (10000, 1000)
+            _, indices = self.knn.kneighbors(user_vectors, n_neighbors=self.n_neighbors)  # shape (10000, k)
+            # movies = self.movie_embeddings  # shape (10000, k, 1000)
+            # movies[movies == 0] = np.nan
+            # user_mean_values = np.nanmean(movies, axis=1)  # shape (10000, 1000) (average over the 5 neighbors)
+            low_rank = self.reconstructed_matrix
+           # np.copyto(low_rank, self.data_wrapper.data_matrix, where=self.data_wrapper.mask.astype(bool))
+
+            ratings = low_rank[indices]
+
+            nearest_neighbors_normalized = np.mean(ratings, axis=1)
+            np.copyto(nearest_neighbors_normalized, self.data_wrapper.data_matrix,
+                      where=self.data_wrapper.mask.astype(bool))
+
+            self.nearest_neighbors = self.transformer.inverse_transform(nearest_neighbors_normalized)
+            #self.nearest_neighbors = nearest_neighbors_normalized
+
+        else:
+            movie_vectors = self.movie_embeddings  # shape (10000, 1000)
+            _, indices = self.knn.kneighbors(movie_vectors, n_neighbors=self.n_neighbors)  # shape (10000, k)
+            # movies = self.movie_embeddings  # shape (10000, k, 1000)
+            # movies[movies == 0] = np.nan
+            # user_mean_values = np.nanmean(movies, axis=1)  # shape (10000, 1000) (average over the 5 neighbors)
+            ratings = np.tensordot(self.movie_embeddings[indices], np.transpose(self.user_embeddings), axes=1)
+            self.nearest_neighbors = np.mean(ratings, axis=1)
+
+    def fit(self, data_wrapper):
+        self.data_wrapper = data_wrapper
+        # data = data_wrapper.data_matrix
+        # data = self.transformer.fit_transform(data)
+
+        self.svd.fit(data_wrapper)
+
+        self.reconstructed_matrix = self.svd.reconstructed_matrix
+
+        self.reconstructed_matrix = self.transformer.fit_transform(self.reconstructed_matrix)
+
+        if self.user_based:
+            self.knn.fit(self.reconstructed_matrix)
+
+        else:
+            self.knn.fit(self.reconstructed_matrix.T)
+
+        self.compute_neighbors()
+
+    def predict(self, users, movies):
+        self.compute_neighbors()
+
+        if self.user_based:
+            predictions = self.nearest_neighbors[tuple([users, movies])]
+        else:
+            predictions = self.nearest_neighbors[tuple([movies, users])]
+
+        nan_mask = np.isnan(predictions)
+        counter_nan = np.count_nonzero(nan_mask)
+
+        np.copyto(predictions, self.data_wrapper.movie_means[movies], where=nan_mask)  # impute nan values
+
+        if (counter_nan > 0):
+            print("** had to impute ", counter_nan, " movies **")
+
+        return predictions
+
+
+class KNNSVD_Embeddings(AlgoBase):
+    """ Prediction based on dimensionality reduction through singular value decomposition """
+
+    def __init__(self, k=5, user_based=True, metric='cosine', algorithm='auto', n_neighbors=5, track_to_comet=False,
+                 method_name=None,
+                 api_key="rISpuwcLQoWU6qan4jRCAPy5s",
+                 projectname="cil-experiments", workspace="veroniquek", tag="baseline"):
+        AlgoBase.__init__(self, track_to_comet=track_to_comet, method_name=method_name, api_key=api_key,
+                          projectname=projectname, workspace=workspace, tag=tag)
+
+        self.n_neighbors = n_neighbors
+        self.knn = NearestNeighbors(metric=metric, algorithm=algorithm, n_neighbors=n_neighbors, n_jobs=-1)
+        self.data_wrapper = None
+        self.nearest_neighbors = None
+
+        self.k = k
+        self.user_based = user_based
+
+        # self.transformer = RobustScaler(with_centering=False, with_scaling=False, unit_variance=False) # 1.0277 mean rsme
+        self.transformer = PowerTransformer()
+        print("using power transformer")
+
+    def compute_neighbors(self):
+
+        if self.user_based:
+            user_vectors = self.user_embeddings  # shape (10000, 1000)
+            _, indices = self.knn.kneighbors(user_vectors, n_neighbors=self.n_neighbors)  # shape (10000, k)
+            # movies = self.movie_embeddings  # shape (10000, k, 1000)
+            # movies[movies == 0] = np.nan
+            # user_mean_values = np.nanmean(movies, axis=1)  # shape (10000, 1000) (average over the 5 neighbors)
+            low_rank = self.user_embeddings.dot(self.movie_embeddings.T)
+            np.copyto(low_rank, self.data_wrapper.data_matrix, where=self.data_wrapper.mask.astype(bool))
+
+            ratings = low_rank[indices]
+
+            nearest_neighbors_normalized = np.mean(ratings, axis=1)
+            np.copyto(nearest_neighbors_normalized, self.data_wrapper.data_matrix,
+                      where=self.data_wrapper.mask.astype(bool))
+
+            self.nearest_neighbors = self.transformer.inverse_transform(nearest_neighbors_normalized)
+
+
+        else:
+            movie_vectors = self.movie_embeddings  # shape (10000, 1000)
+            _, indices = self.knn.kneighbors(movie_vectors, n_neighbors=self.n_neighbors)  # shape (10000, k)
+            # movies = self.movie_embeddings  # shape (10000, k, 1000)
+            # movies[movies == 0] = np.nan
+            # user_mean_values = np.nanmean(movies, axis=1)  # shape (10000, 1000) (average over the 5 neighbors)
+            ratings = np.tensordot(self.movie_embeddings[indices], np.transpose(self.user_embeddings), axes=1)
+            self.nearest_neighbors = np.mean(ratings, axis=1)
+
+    def fit(self, data_wrapper):
+        self.data_wrapper = data_wrapper
+        data = data_wrapper.data_matrix
+        data = self.transformer.fit_transform(data)
+
+        self.user_embeddings, self.movie_embeddings = SVD.get_embeddings(self.k, data)
+
+        if self.user_based:
+            self.knn.fit(self.user_embeddings)
+
+        else:
+            self.knn.fit(self.movie_embeddings)
+
+        self.compute_neighbors()
+
+    def predict(self, users, movies):
+        self.compute_neighbors()
+
+        if self.user_based:
+            predictions = self.nearest_neighbors[tuple([users, movies])]
+        else:
+            predictions = self.nearest_neighbors[tuple([movies, users])]
+
+        nan_mask = np.isnan(predictions)
+        counter_nan = np.count_nonzero(nan_mask)
+
+        np.copyto(predictions, self.data_wrapper.movie_means[movies], where=nan_mask)  # impute nan values
+
+        if (counter_nan > 0):
+            print("** had to impute ", counter_nan, " movies **")
+
+        return predictions
 
 
 class KNNUserMovie(AlgoBase):
     """ Prediction based on dimensionality reduction through singular value decomposition """
 
-    def __init__(self, user_metric='cosine', user_algorithm='auto', movie_metric='cosine', movie_algorithm='auto', n_user_neighbors=5,  n_movie_neighbors=5, track_to_comet=False, method_name=None,
+    def __init__(self, user_metric='cosine', user_algorithm='auto', movie_metric='cosine', movie_algorithm='auto',
+                 n_user_neighbors=5, n_movie_neighbors=5, track_to_comet=False, method_name=None,
                  api_key="rISpuwcLQoWU6qan4jRCAPy5s",
                  projectname="cil-experiments", workspace="veroniquek", tag="baseline"):
         AlgoBase.__init__(self, track_to_comet=track_to_comet, method_name=method_name, api_key=api_key,
@@ -28,10 +206,11 @@ class KNNUserMovie(AlgoBase):
 
         self.n_user_neighbors = n_user_neighbors
         self.n_movie_neighbors = n_movie_neighbors
-        self.movie_knn = NearestNeighbors(metric=movie_metric, algorithm=movie_algorithm, n_neighbors=n_movie_neighbors, n_jobs=-1)
-        self.user_knn = NearestNeighbors(metric=user_metric, algorithm=user_algorithm, n_neighbors=n_user_neighbors, n_jobs=-1)
+        self.movie_knn = NearestNeighbors(metric=movie_metric, algorithm=movie_algorithm, n_neighbors=n_movie_neighbors,
+                                          n_jobs=-1)
+        self.user_knn = NearestNeighbors(metric=user_metric, algorithm=user_algorithm, n_neighbors=n_user_neighbors,
+                                         n_jobs=-1)
         self.knn_euclidean = KNN(metric='euclidean', algorithm='auto', n_neighbors=500)
-
 
         self.data_wrapper = None
         self.movie_nearest_neighbors = None
@@ -68,7 +247,6 @@ class KNNUserMovie(AlgoBase):
         self.movie_knn.fit(movie_matrix)
         self.compute_neighbors()
 
-
     def predict(self, users, movies):
 
         user_predictions = self.user_nearest_neighbors[tuple([users, movies])]
@@ -78,7 +256,6 @@ class KNNUserMovie(AlgoBase):
         mask_either_is_nan = np.logical_or(np.isnan(user_predictions), np.isnan(movie_predictions))
         predictions = movie_predictions
         # predictions[np.isnan(user_predictions)] = movie_predictions[np.isnan(user_predictions)]
-
 
         # predictions[~mask_either_is_nan] = self.user_weight*user_predictions[~mask_either_is_nan] + (1-self.user_weight)*movie_predictions[~mask_either_is_nan]
         # predictions[predictions < 3.5] = euclidean_user_predictions[predictions < 3.5]
@@ -97,6 +274,7 @@ class KNNUserMovie(AlgoBase):
 
         print("had to impute ", counter_nan, " movies")
         return predictions
+
 
 class KNNBag(AlgoBase):
     def __init__(self, knns=None, track_to_comet=False, method_name=None, api_key="rISpuwcLQoWU6qan4jRCAPy5s",
@@ -137,9 +315,6 @@ class KNNBag(AlgoBase):
 
         predictions[predictions >= 3.5] = predictions_cosine[predictions >= 3.5]
 
-
-
-
         # fill predictions that are STILL nan with the mean
         counter_nan = 0
         for i, pred in enumerate(predictions):
@@ -148,7 +323,8 @@ class KNNBag(AlgoBase):
                 counter_nan += 1
         print("In the end, we had to impute ", counter_nan, " of the predictions with the movie-mean")
 
-        predictions = predictions + self.data_wrapper.movie_bias[movies] + self.data_wrapper.user_bias[users]   # include biases
+        predictions = predictions + self.data_wrapper.movie_bias[movies] + self.data_wrapper.user_bias[
+            users]  # include biases
 
         return predictions
 
@@ -159,7 +335,6 @@ class KNNBag(AlgoBase):
         # self.knn_manhattan = KNN(metric='manhattan', algorithm='auto', n_neighbors=2)
         self.knn_euclidean = KNN(metric='euclidean', algorithm='auto', n_neighbors=500)
         self.knns = [self.knn_cosine, self.knn_euclidean]
-
 
 
 class KNN(AlgoBase):
@@ -175,7 +350,6 @@ class KNN(AlgoBase):
         self.knn = NearestNeighbors(metric=metric, algorithm=algorithm, n_neighbors=n_neighbors, n_jobs=-1)
         self.data_wrapper = None
         self.nearest_neighbors = None
-
 
         self.user_based = True
         # self.item_based = False
@@ -194,7 +368,7 @@ class KNN(AlgoBase):
         else:
             movie_vectors = self.data_wrapper.user_per_movie_encodings  # shape (10000, 1000)
             _, indices = self.knn.kneighbors(movie_vectors,
-                                                   n_neighbors=self.n_neighbors)  # shape (10000, k)
+                                             n_neighbors=self.n_neighbors)  # shape (10000, k)
             users = self.data_wrapper.user_per_movie_encodings[indices]  # shape (10000, k, 1000)
             users[users == 0] = np.nan
             movie_mean_values = np.nanmean(users, axis=1)  # shape (10000, 1000) (average over the 5 neighbors)
@@ -211,12 +385,10 @@ class KNN(AlgoBase):
     def predict(self, users, movies):
         return self.predict_movie_mean(users, movies)
 
-
-
     def predict_movie_mean(self, users, movies):
         self.compute_neighbors()
 
-        if(self.user_based):
+        if (self.user_based):
             predictions = self.nearest_neighbors[tuple([users, movies])]
         else:
             predictions = self.nearest_neighbors[tuple([movies, users])]
@@ -234,5 +406,3 @@ class KNN(AlgoBase):
         print("In the end, we had to impute ", counter_nan, " of the predictions with the movie-mean")
 
         return predictions
-
-
