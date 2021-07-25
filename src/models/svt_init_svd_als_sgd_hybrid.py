447e3eb1b40c0b386ddfe7127daf8ec2557c06f1
milojal1 <aleksandar.milojevic@gmail.com>
2021-07-25 11:10:39 +0200

    add custom cross validation to model

--- src/models/svt_init_svd_als_sgd_hybrid.py
+++ src/models/svt_init_svd_als_sgd_hybrid.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from utils import data_processing
 from models.algobase import AlgoBase
@@ -7,6 +8,7 @@ from torch.utils.tensorboard import SummaryWriter
 import time
 from tqdm import tqdm
 from collections import defaultdict
+from sklearn.model_selection import KFold
 
 EPSILON = 1e-5
 
@@ -37,6 +39,13 @@ class SVT_INIT_SVD_ALS_SGD(AlgoBase):
         self.bi = np.zeros(self.number_of_movies)  # item bias
 
         self.directory_path = data_processing.get_project_directory()
+        self.cv_svt_matrix_filenames = ['svt_Xopt_Yk_sh100k_0_to_2000_CV_20210723-005714.npy',
+        'svt_Xopt_Yk_sh100k_0_to_2000_CV_20210723-080627.npy',
+        'svt_Xopt_Yk_sh100k_0_to_2000_CV_20210723-151440.npy',
+        'svt_Xopt_Yk_sh100k_0_to_2000_CV_20210723-222641.npy',
+        'svt_Xopt_Yk_sh100k_0_to_2000_CV_20210724-054423.npy']
+        self.svt_init_matrix_path = ''
+        self.svt_matrix = np.zeros((self.number_of_users, self.number_of_movies))
 
         self.mu = 0
 
@@ -81,16 +90,18 @@ class SVT_INIT_SVD_ALS_SGD(AlgoBase):
 
     def fit(self, users, movies, ground_truth, valid_users=None, valid_movies=None, valid_ground_truth=None):
         
-        with open(self.directory_path + '/data/svt_Xopt_Yk_sh100k_0_to_2000_Submit.npy', 'rb') as f:
-            self.matrix = np.load(f, allow_pickle=True)
-            Yk = np.load(f, allow_pickle=True)
+        with open(self.directory_path + self.svt_init_matrix_path, 'rb') as f:
+            self.svt_matrix = np.load(f, allow_pickle=True)
+            Yk = np.load(f, allow_pickle=True) #this is not used but in the file since it was needed for svt
+        
+        self.matrix, _ = data_processing.get_data_mask(users, movies, ground_truth)
 
         (ur, ir) = self.create_adjacency_lists(users, movies, ground_truth)
         self.fit_model_baseline_als(ur, ir, 50)
 
-        self.pu, self.qi = SVD.get_embeddings(self.k, self.matrix)
+        self.pu, self.qi = SVD.get_embeddings(self.k, self.svt_matrix)
 
-        run_validation = valid_users is not None and valid_movies is not None and valid_ground_truth is not None
+        run_validation = False
         indices = np.arange(len(users))
         # global_mean = np.mean(self.matrix[np.nonzero(self.matrix)])
 
@@ -131,6 +142,43 @@ class SVT_INIT_SVD_ALS_SGD(AlgoBase):
         predictions[predictions > 5] = 5
         predictions[predictions < 1] = 1
         return predictions
+    
+    def cross_validate(self, data_pd, folds=5, random_state=42):
+        """ Run Crossvalidation using kfold, taking a pandas-dataframe of the raw data as input
+            (as it is read in from the .csv file) """
+        kfold = KFold(n_splits=folds, shuffle=True, random_state=random_state)
+
+        rmses = []
+
+        bar = tqdm(total=folds,  desc='cross_validation')
+        counter = 0
+        for train_index, test_index in kfold.split(data_pd):
+            train_users, train_movies, train_predictions = data_processing.extract_users_items_predictions(
+                data_pd.iloc[train_index])
+            val_users, val_movies, val_predictions = data_processing.extract_users_items_predictions(
+                data_pd.iloc[test_index])
+
+            self.svt_init_matrix = self.cv_svt_matrix_filenames[counter]
+            self.fit(train_users, train_movies, train_predictions, val_users, val_movies, val_predictions)
+            counter += 1
+
+            predictions = self.predict(val_users, val_movies)
+            rmses.append(data_processing.get_score(predictions, val_predictions))
+
+            bar.update()
+
+        bar.close()
+
+        mean_rmse = np.mean(rmses)
+        # track mean rmses to comet if we are tracking
+        if self.track_on_comet:
+            self.comet_experiment.log_metrics(
+                {
+                    "root_mean_squared_error": mean_rmse
+                }
+            )
+        print(rmses)
+        return rmses
 
 
 if __name__ == '__main__':
@@ -138,7 +186,7 @@ if __name__ == '__main__':
     k = 12
     epochs = 43
 
-    submit = True
+    submit = False
 
     svt_init_svd_hybrid = SVT_INIT_SVD_ALS_SGD(k_singular_values=k, epochs=epochs, verbal=True)
 
@@ -147,7 +195,5 @@ if __name__ == '__main__':
         svt_init_svd_hybrid.fit(users, movies, predictions)
         svt_init_svd_hybrid.predict_for_submission(f'svt_init_svd_als_sgd_k{k}_{epochs}')
     else:
-        train_pd, test_pd = train_test_split(data_pd, train_size=0.9, random_state=42)
-        users, movies, predictions = data_processing.extract_users_items_predictions(train_pd)
-        val_users, val_movies, val_predictions = data_processing.extract_users_items_predictions(test_pd)
-        svt_init_svd_hybrid.fit(users, movies, predictions, val_users, val_movies, val_predictions)
+        rmses = svt_init_svd_hybrid.cross_validate(data_pd)
+        print("RMSES of ", svt_init_svd_hybrid.method_name, "\n", rmses, "\n")
